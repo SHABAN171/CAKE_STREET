ef58a2711ce02bcf3f09bfc606fef9b04112570a
SHABAN171 <shabanindekupe@gmail.com>
2025-12-14 17:37:22 +0300

    views file for bakery created

--- bakery/views.py
+++ bakery/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Cake, Order
+from .serializers import CategorySerializer, CakeSerializer, OrderSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CakeViewSet(viewsets.ModelViewSet):
+    queryset = Cake.objects.all()
+    serializer_class = CakeSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
