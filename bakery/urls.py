0861b7b781008df525affa70c245715c9755d06c
SHABAN171 <Shabanindekupe@gmail.com>
2026-02-11 22:46:48 +0300

    unnecessary files were deleted and also backend files were added

--- bakery/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, CakeViewSet, OrderViewSet
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'cakes', CakeViewSet)
-router.register(r'orders', OrderViewSet)
-
-urlpatterns = router.urls
